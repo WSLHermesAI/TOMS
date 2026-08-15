0b512693866bbd27d0968ed5ed50e4265f20bb9f
Fatming Wang <fatming@wsl.local>
2026-08-15 08:57:01 +0800

    Add .gitignore (build dirs, runtime logs, frames) and untrack stray toms.log

---
+++ build-webgpu/CMakeFiles/node_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/node_test.dir/src/node.cpp.o"
+  "CMakeFiles/node_test.dir/src/node.cpp.o.d"
+  "CMakeFiles/node_test.dir/src/node_test.cpp.o"
+  "CMakeFiles/node_test.dir/src/node_test.cpp.o.d"
+  "node_test.js"
+  "node_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/node_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-webgpu/CMakeFiles/tower_vulkan_web.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for tower_vulkan_web.
+# This may be replaced when dependencies are built.
