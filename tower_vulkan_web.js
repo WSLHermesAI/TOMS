eef470cfed3f41665d76dcff0391f34ce3aa4ab8
TOMS Build <build@toms.local>
2026-08-16 18:17:58 +0800

    Fix web crash: EM_ASM raw-string (R"JS") leaked invalid JS; use emscripten_run_script with plain string literal

--- tower_vulkan_web.js
+++ tower_vulkan_web.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpmdpq46np.js
+// include: /tmp/tmpdg7x0w73.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -209,21 +209,21 @@ Module['FS_createPath']("/data", "stages", true, true);
 
   })();
 
-// end include: /tmp/tmpmdpq46np.js
-// include: /tmp/tmpbig1p7l7.js
+// end include: /tmp/tmpdg7x0w73.js
+// include: /tmp/tmp2l0_xi6d.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpbig1p7l7.js
-// include: /tmp/tmp4hgdhwis.js
+  // end include: /tmp/tmp2l0_xi6d.js
+// include: /tmp/tmppl0adbmt.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp4hgdhwis.js
+  // end include: /tmp/tmppl0adbmt.js
 
 
 var programArgs = [];
