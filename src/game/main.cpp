4d908328ec04b0a463117f2ff457745fdde6da2d
Fatming <osimejp@yahoo.co.jp>
2026-08-17 18:44:49 +0800

    change shader directory

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -44,7 +44,6 @@ int main(int argc, char** argv) {
 #else
     // frames output dir (relative to CWD); create portably
     std::string framesDir = "frames";
-
 #endif
     fs::create_directories(framesDir, ec);
 
