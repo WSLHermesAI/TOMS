127c3e6a83d534eb10b953e8e9695e21a9b313eb
HermesAI <leeyoungaeo@yahoo.com.tw>
2026-09-13 19:18:05 +0800

    Web build: S3.5 (c)(d)(e), stamp db128ea-20260913191337

--- web/toms_web.js
+++ web/toms_web.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpwgzctn5c.js
+// include: /tmp/tmp6rui9vb_.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -214,21 +214,21 @@ Module['FS_createPath']("/data/story", "floors", true, true);
 
   })();
 
-// end include: /tmp/tmpwgzctn5c.js
-// include: /tmp/tmpsaxw9khr.js
+// end include: /tmp/tmp6rui9vb_.js
+// include: /tmp/tmp4s14dgnz.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpsaxw9khr.js
-// include: /tmp/tmp4tmtoodz.js
+  // end include: /tmp/tmp4s14dgnz.js
+// include: /tmp/tmpeyzeai5m.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp4tmtoodz.js
+  // end include: /tmp/tmpeyzeai5m.js
 
 
 var programArgs = [];
